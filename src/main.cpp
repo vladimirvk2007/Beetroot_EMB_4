9e45ee768da20238a25657a03e656ea33e5e831b
Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>
2026-03-07 01:49:51 +0100

    Motor control
    Signed-off-by: Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,32 @@
 #include <Arduino.h>
 
-#include "ota.h"
+#define POT_PIN 4    // GPIO4 - потенціометр (ADC)
+#define PWM_PIN 15   // GPIO15 - вихід ШІМ
 
 void setup() {
     Serial.begin(115200);
-
-    otaSetup();
+    pinMode(PWM_PIN, OUTPUT);
+    analogReadResolution(12); // 12 біт
+    analogSetAttenuation(ADC_11db); // максимальний діапазон
 }
 
 void loop() {
-    otaHandle();
+    // Зчитуємо значення з потенціометра (12 біт, 0..4095)
+    int adc = analogRead(POT_PIN);
+    // Перетворюємо у 10 градацій (0..10)
+    int level = map(adc, 0, 4095, 0, 10);
+
+    // Виводимо значення в серійний монітор
+    Serial.printf("ADC: %d  Level: %d\n", adc, level);
+
+    // Програмний ШІМ: 10 тактів (градацій)
+    for (int i = 0; i < 10; ++i) {
+        if (i < level) {
+            digitalWrite(PWM_PIN, HIGH);
+        } else {
+            digitalWrite(PWM_PIN, LOW);
+        }
+        delayMicroseconds(100); // Тривалість одного такту (можна підібрати)
+    }
 }
 
