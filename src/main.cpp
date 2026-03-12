f5d5eb6635c8d439b09d7c47f02b204d9bc83eac
Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>
2026-03-12 17:48:44 +0100

    Add start - stop timer
    Signed-off-by: Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,60 @@
 #include <Arduino.h>
 #include <atomic>
+#include <OneButton.h>
 
 #define BTN_STOP_ALARM 15
 
 std::atomic<uint32_t> isrCounter(0);
 volatile bool timerFired = false;
+hw_timer_t *timer = nullptr;
+bool timerActive = false;
 
-hw_timer_t *timer = NULL;
-
-// Функція переривання (ISR)
 void ARDUINO_ISR_ATTR onTimer() {
-	// Операції ++ та присвоєння для atomic є безпечними (атомарними)
-	isrCounter++;
+	isrCounter.fetch_add(1, std::memory_order_relaxed);
 	timerFired = true;
+}
+
+OneButton button(BTN_STOP_ALARM, true, true);
+
+void startTimer() {
+	if (!timerActive) {
+		timer = timerBegin(0, 80, true);
+		timerAttachInterrupt(timer, &onTimer, true);
+		timerAlarmWrite(timer, 1000000, true);
+		timerAlarmEnable(timer);
+		timerActive = true;
+		Serial.println("Timer started...");
+	}
+}
+
+void stopTimer() {
+	if (timerActive && timer) {
+		timerDetachInterrupt(timer);
+		timerEnd(timer);
+		timer = nullptr;
+		timerActive = false;
+		Serial.println("!!! Timer stopped by user !!!");
+	}
+}
+
+void toggleTimer() {
+	if (timerActive) {
+		stopTimer();
+	} else {
+		startTimer();
+	}
 }
 
 void setup() {
 	Serial.begin(115200);
-
-	pinMode(BTN_STOP_ALARM, INPUT_PULLUP);
-
-	// Ініціалізація таймера (ESP32-S3)
-	// divider = 80: 80 МГц / 80 = 1 МГц (1 tick = 1 мкс)
-	timer = timerBegin(0, 80, true);
-
-	// Прив'язка функції переривання
-	timerAttachInterrupt(timer, &onTimer, true);
-
-	// Налаштування спрацювання: 1 000 000 мікросекунд = 1 секунда
-	timerAlarmWrite(timer, 1000000, true);
-	timerAlarmEnable(timer);
-
-	Serial.println("Timer started...");
+	button.attachClick(toggleTimer);
+	startTimer();
 }
 
 void loop() {
 	if (timerFired) {
 		timerFired = false;
-
-		// Safely read the counter value
 		uint32_t currentCount = isrCounter.load();
-
 		Serial.print("Trigger #: ");
 		Serial.print(currentCount);
 		Serial.print(" | Time: ");
@@ -48,15 +62,8 @@ void loop() {
 		Serial.println(" ms");
 	}
 
-	// Перевірка натискання кнопки для зупинки
-	if (digitalRead(BTN_STOP_ALARM) == LOW) {
-		if (timer) {
-			timerEnd(timer);
-			timer = NULL;
-			Serial.println("!!! Timer stopped by user !!!");
-		}
-	}
+	button.tick();
 
-	delay(10);
+	delay(1);
 }
 
