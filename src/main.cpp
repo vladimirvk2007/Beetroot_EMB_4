4438d3924f8076d81f0cc4eef82d523d739ed55f
maksymrud <maksymrud607@gmail.com>
2026-03-14 22:36:35 +0200

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,20 @@
 #include <Arduino.h>
 
 enum class LedState {
-  OFF,
-  ON,
-  Blinking
+    OFF,
+    ON,
+    Blinking
 };
 
 class Config {
     private:
-        uint16_t pulse_width;
-        LedState states[3] = {LedState::Blinking, LedState::ON, LedState::OFF};
         constexpr static uint16_t delay_ms = 1000;
-        constexpr static uint16_t pulse_widths[] = {100, 500, 1000, 2000};
-        constexpr static uint8_t pulse_widths_array_size = sizeof(pulse_widths) / sizeof(pulse_widths[0]);
 
     public:
-        Config() : pulse_width(pulse_widths[0]) {}
+        Config() {}
 
-        void updatePulseWidths(uint8_t mode) {
-            uint8_t index = mode % pulse_widths_array_size;
-            this->pulse_width = pulse_widths[index];
-        }
-
-        uint16_t getPulseWidth() const {
-            return this->pulse_width;
-        }
-
-
-        uint16_t getDelayMs() const {
-            return this->delay_ms;
+        static uint16_t getDelayMs() {
+            return delay_ms;
         }
 };
 
@@ -38,7 +24,7 @@ class Led {
         uint8_t pin;
         uint16_t blink_delay;
 
-        void setState(LedState new_state) {
+        volatile void setState(LedState new_state) {
 
             switch(new_state) {
                 case LedState::OFF:
@@ -48,7 +34,7 @@ class Led {
                     digitalWrite(this->pin, HIGH);
                     break;
                 case LedState::Blinking:
-                    while (1)
+                    while (button_pressed == false)
                     {
                         unsigned long current_time = millis();
                         this->on();
@@ -57,7 +43,7 @@ class Led {
                         }
                         this->off();
                         current_time = millis();
-                        while (millis() - current_time < blink_delay) {
+                        while (millis() - current_time < Config::getDelayMs()) {
                             // Wait for the specified low pulse width
                         }
                     }
@@ -67,7 +53,7 @@ class Led {
         }
 
     public:
-        Led(uint8_t pin, uint16_t blink_delay) : state(LedState::OFF), pin(pin), blink_delay(blink_delay) {
+        Led(uint8_t pin) : state(LedState::OFF), pin(pin) {
             pinMode(pin, OUTPUT);
         }
 
@@ -82,25 +68,54 @@ class Led {
         void blink() {
             this->setState(LedState::Blinking);
         }
+
+        LedState getState() {
+            return this->state;
+        }
 };
 
 constexpr uint8_t button_mode_up_pin = 5;
 constexpr uint8_t green_led_pin = 13;
 constexpr uint8_t yellow_led_pin = 4;
+unsigned long lastInterruptTime = 0;
 volatile bool button_pressed = false;
-Config config;
-Led greenLed(green_led_pin, config.getDelayMs());
-Led yellowLed(yellow_led_pin, config.getDelayMs());
+Led greenLed;
+Led yellowLed;
 
-void IRAM_ATTR reaction_left() {
-  button_pressed = true;
+void IRAM_ATTR button_pressed_isr() {
+    unsigned long currentTime = millis();
+
+    // Ігноруємо переривання, що надходять занадто часто (брязкіт)
+    if (currentTime - lastInterruptTime > 50) {
+        lastInterruptTime = currentTime;
+        button_pressed = true;
+    }
 }
 
 void setup() {
   pinMode(button_mode_up_pin, INPUT_PULLDOWN);
   Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(button_mode_up_pin), reaction_left, RISING);
+  greenLed = Led(green_led_pin);
+  yellowLed = Led(yellow_led_pin);
+  attachInterrupt(digitalPinToInterrupt(button_mode_up_pin), button_pressed_isr, RISING);
 }
 
 void loop() {
+    if (button_pressed) {
+        Serial.println("Button pressed!");
+        button_pressed = false;
+
+        switch (greenLed.getState())
+        {
+            case LedState::OFF:
+                greenLed.blink();
+                break;
+            case LedState::ON:
+                greenLed.off();
+                break;
+            case LedState::Blinking:
+                greenLed.on();
+                break;
+        }
+    }
 }
