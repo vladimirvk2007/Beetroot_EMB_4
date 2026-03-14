a31194b72dd6960d5eb0712d941d52526db9d9e1
maksymrud <maksymrud607@gmail.com>
2026-03-14 20:23:59 +0200

    some fixes

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,30 @@
 
 enum class LedState {
   OFF,
-  ON
+  ON,
+  Blinking
 };
 
 class Config {
     private:
-        uint16_t pulse_width_high;
-        uint16_t pulse_width_low;
-        uint16_t delay_ms = 1000;
+        uint16_t pulse_width;
+        LedState states[3] = {LedState::Blinking, LedState::ON, LedState::OFF};
+        constexpr static uint16_t delay_ms = 1000;
         constexpr static uint16_t pulse_widths[] = {100, 500, 1000, 2000};
         constexpr static uint8_t pulse_widths_array_size = sizeof(pulse_widths) / sizeof(pulse_widths[0]);
 
     public:
-        Config() : pulse_width_high(pulse_widths[0]), pulse_width_low(pulse_widths[0]) {}
+        Config() : pulse_width(pulse_widths[0]) {}
 
         void updatePulseWidths(uint8_t mode) {
             uint8_t index = mode % pulse_widths_array_size;
-            this->pulse_width_high = pulse_widths[index];
-            this->pulse_width_low = pulse_widths[index];
+            this->pulse_width = pulse_widths[index];
         }
 
-        uint16_t getPulseWidthHigh() const {
-            return this->pulse_width_high;
+        uint16_t getPulseWidth() const {
+            return this->pulse_width;
         }
 
-        uint16_t getPulseWidthLow() const {
-            return this->pulse_width_low;
-        }
 
         uint16_t getDelayMs() const {
             return this->delay_ms;
@@ -39,18 +36,38 @@ class Led {
     private:
         LedState state;
         uint8_t pin;
+        uint16_t blink_delay;
 
         void setState(LedState new_state) {
-            if (new_state == LedState::ON) {
-                digitalWrite(this->pin, HIGH);
-            } else {
-                digitalWrite(this->pin, LOW);
-            }
+
+            switch(new_state) {
+                case LedState::OFF:
+                    digitalWrite(this->pin, LOW);
+                    break;
+                case LedState::ON:
+                    digitalWrite(this->pin, HIGH);
+                    break;
+                case LedState::Blinking:
+                    while (1)
+                    {
+                        unsigned long current_time = millis();
+                        this->on();
+                        while (millis() - current_time < blink_delay) {
+                            // Wait for the specified high pulse width
+                        }
+                        this->off();
+                        current_time = millis();
+                        while (millis() - current_time < blink_delay) {
+                            // Wait for the specified low pulse width
+                        }
+                    }
+                    break;
+            };
             this->state = new_state;
         }
 
     public:
-        Led(uint8_t pin) : state(LedState::OFF), pin(pin) {
+        Led(uint8_t pin, uint16_t blink_delay) : state(LedState::OFF), pin(pin), blink_delay(blink_delay) {
             pinMode(pin, OUTPUT);
         }
 
@@ -62,57 +79,28 @@ class Led {
             this->setState(LedState::OFF);
         }
 
-        void blink(uint16_t pulse_width_high, uint16_t pulse_width_low) {
-            unsigned long current_time = millis();
-            this->on();
-            while (millis() - current_time < pulse_width_high) {
-                // Wait for the specified high pulse width
-            }
-            this->off();
-            current_time = millis();
-            while (millis() - current_time < pulse_width_low) {
-                // Wait for the specified low pulse width
-            }
+        void blink() {
+            this->setState(LedState::Blinking);
         }
 };
 
-constexpr uint16_t delay_ms = 1000;
-constexpr uint8_t button_mode_up = 5;
-constexpr uint8_t green_led = 13;
-constexpr uint8_t yellow_led = 4;
+constexpr uint8_t button_mode_up_pin = 5;
+constexpr uint8_t green_led_pin = 13;
+constexpr uint8_t yellow_led_pin = 4;
 volatile bool button_pressed = false;
-Led greenLed(green_led);
-Led yellowLed(yellow_led);
 Config config;
+Led greenLed(green_led_pin, config.getDelayMs());
+Led yellowLed(yellow_led_pin, config.getDelayMs());
 
 void IRAM_ATTR reaction_left() {
   button_pressed = true;
 }
 
 void setup() {
-  pinMode(button_mode_up, INPUT_PULLDOWN);
+  pinMode(button_mode_up_pin, INPUT_PULLDOWN);
   Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(button_mode_up), reaction_left, RISING);
+  attachInterrupt(digitalPinToInterrupt(button_mode_up_pin), reaction_left, RISING);
 }
 
 void loop() {
-  if (button_pressed) {
-    button_pressed = false;
-    yellowLed.on();
-    unsigned long current_time = millis();
-    yellowLed.on();
-    greenLed.on();
-    while (millis() - current_time < config.getDelayMs()) {
-        // Wait for the specified high pulse width
-    }
-    yellowLed.off();
-    greenLed.off();
-    current_time = millis();
-    while (millis() - current_time < config.getDelayMs()) {
-        // Wait for the specified low pulse width
-    }
-  }
-
-  greenLed.blink(config.getPulseWidthHigh(), config.getPulseWidthLow());
-  yellowLed.blink(config.getPulseWidthHigh(), config.getPulseWidthLow());
 }
