90447c951ead54385ca8fda8076dc794f6f5b489
Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>
2026-03-21 15:53:14 +0100

    Add GPIO_14 output
    Signed-off-by: Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -100,6 +100,7 @@ int main(void)
     const char *msg = "LED blinked!\r\n";
 
 	  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_14);
 
 	  CDC_Transmit_FS((uint8_t*)msg, strlen(msg));
 
@@ -174,10 +175,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13|GPIO_PIN_14, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PC13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  /*Configure GPIO pins : PC13 PC14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
