6c253e8ec8a51abef8621c46dc61fb9af44c0620
Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>
2026-03-27 20:43:30 +0100

    Add ADC1 measurement
    Signed-off-by: Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>

--- src/Core/Src/main_app.cpp
+++ src/Core/Src/main_app.cpp
@@ -2,6 +2,8 @@
 #include "main.h"
 #include "usbd_cdc_if.h"
 
+extern ADC_HandleTypeDef hadc1;
+
 class Led {
 public:
     Led(GPIO_TypeDef* port, uint16_t pin) : port_(port), pin_(pin) {}
@@ -13,17 +15,25 @@ private:
     const uint16_t pin_;
 };
 
-extern "C" {
-
-void main_cpp() {
-    const char *msg = "LED blinked!\r\n";
+extern "C" void main_cpp() {
     Led led13(GPIOC, GPIO_PIN_13);
+
+    char adc_msg[64];
+    uint32_t adc_val = 0;
 
     while(1) {
         led13.toggle();
-        CDC_Transmit_FS((uint8_t*)msg, strlen(msg));
+        // Start ADC conversion
+        HAL_ADC_Start(&hadc1);
+        if (HAL_ADC_PollForConversion(&hadc1, 100) == HAL_OK) {
+            adc_val = HAL_ADC_GetValue(&hadc1);
+        }
+        HAL_ADC_Stop(&hadc1);
+
+        // Send ADC value over USB CDC
+        int len = snprintf(adc_msg, sizeof(adc_msg), "ADC1_CH1: %lu\r\n", adc_val);
+        CDC_Transmit_FS((uint8_t*)adc_msg, len);
+
         HAL_Delay(2000);
     }
-}
-
 }
