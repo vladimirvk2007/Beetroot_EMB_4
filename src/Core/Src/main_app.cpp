147b89fe11d4234ec2259e45567585163f11beb7
Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>
2026-04-14 17:36:08 +0200

    Add UART processing
    Signed-off-by: Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>

--- src/Core/Src/main_app.cpp
+++ src/Core/Src/main_app.cpp
@@ -2,6 +2,8 @@
 #include "main.h"
 #include "usbd_cdc_if.h"
 
+extern UART_HandleTypeDef huart2;  // USART2: PA3 (RX), PA2 (TX)
+
 class Led {
 public:
     Led(GPIO_TypeDef* port, uint16_t pin) : port_(port), pin_(pin) {}
@@ -16,10 +18,19 @@ private:
 extern "C" void main_cpp() {
     const char *msg = "LED blinked!\r\n";
     Led led13(GPIOC, GPIO_PIN_13);
+    uint8_t rxData[1];
 
     while(1) {
+        // Неблокуючий прийом із таймаутом 0 (проверка без очікування)
+        if (HAL_UART_Receive(&huart2, rxData, 1, 0) == HAL_OK) {
+            // Отримали 1 байт, надсилаємо відповідь
+            HAL_UART_Transmit(&huart2, (uint8_t*)"OK\r\n", 4, 100);
+            CDC_Transmit_FS((uint8_t*)rxData, 1);
+            led13.toggle();
+        }
+
         led13.toggle();
         CDC_Transmit_FS((uint8_t*)msg, strlen(msg));
-        HAL_Delay(2000);
+        HAL_Delay(1000);
     }
 }
