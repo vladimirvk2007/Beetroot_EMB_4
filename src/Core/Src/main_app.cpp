cf9289ee394587037bc45bad1fa8b1d921571912
Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>
2026-04-16 00:16:24 +0200

    Add constants
    Signed-off-by: Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>

--- src/Core/Src/main_app.cpp
+++ src/Core/Src/main_app.cpp
@@ -2,7 +2,12 @@
 #include "main.h"
 #include "usbd_cdc_if.h"
 
-#define MESSAGE_SIZE 48
+#define MESSAGE_SIZE        48
+#define LED_GPIO_PIN        GPIO_PIN_13
+#define LED_GPIO_PORT       GPIOC
+#define UART_TIMEOUT_MS     100  // Таймаут для UART в мілісекундах
+#define UART_BUFFER_SIZE    1    // Розмір буфера для прийому даних з UART
+#define LED_BLINK_DELAY_MS  10   // Затримка для блимання світлодіода в мілісекундах
 
 extern UART_HandleTypeDef huart2;  // USART2: PA3 (RX), PA2 (TX)
 
@@ -24,20 +29,20 @@ private:
 };
 
 extern "C" void main_cpp() {
-    Led led13(GPIOC, GPIO_PIN_13);
+    Led led13(LED_GPIO_PORT, LED_GPIO_PIN);
     uint8_t msg[MESSAGE_SIZE] = {0};
-    uint8_t rxData[1];
+    uint8_t rxData[UART_BUFFER_SIZE];
 
     while(1) {
         // Неблокуючий прийом із таймаутом 0 (проверка без очікування)
-        if (HAL_UART_Receive(&huart2, rxData, 1, 0) == HAL_OK) {
+        if (HAL_UART_Receive(&huart2, rxData, UART_BUFFER_SIZE, 0) == HAL_OK) {
             // Отримали 1 байт
             sprintf((char*)msg, "Received from UART2: 0x%x\r\n", rxData[0]);
             CDC_Transmit_FS(msg, strlen((char*)msg));
             // Відправляємо отриманий байт назад на UART
-            HAL_UART_Transmit(&huart2, (uint8_t*)rxData, 1, 100);
+            HAL_UART_Transmit(&huart2, (uint8_t*)rxData, UART_BUFFER_SIZE, UART_TIMEOUT_MS);
             // Блимання світлодіода для візуального підтвердження отримання даних
-            led13.blink(10);
+            led13.blink(LED_BLINK_DELAY_MS);
         }
     }
 }
