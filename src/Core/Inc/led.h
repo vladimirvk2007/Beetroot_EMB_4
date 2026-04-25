2050ec4d905646723ea4cfe5117fd51d386c6eb0
Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>
2026-04-25 09:04:54 +0200

    Create led.h
    Signed-off-by: Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>

---
+++ src/Core/Inc/led.h
@@ -0,0 +1,15 @@
+#ifndef LED_H
+#define LED_H
+
+class Led {
+public:
+    Led(GPIO_TypeDef* port, uint16_t pin) : port_(port), pin_(pin) {}
+    void toggle() { HAL_GPIO_TogglePin(port_, pin_); }
+    void on()    { HAL_GPIO_WritePin(port_, pin_, GPIO_PIN_SET); }
+    void off()   { HAL_GPIO_WritePin(port_, pin_, GPIO_PIN_RESET); }
+private:
+    GPIO_TypeDef* port_;
+    const uint16_t pin_;
+};
+
+#endif // LED_H
--- src/Core/Src/main_app.cpp
+++ src/Core/Src/main_app.cpp
@@ -1,17 +1,8 @@
 #include <string.h>
 #include "main.h"
 #include "usbd_cdc_if.h"
+#include "led.h"
 
-class Led {
-public:
-    Led(GPIO_TypeDef* port, uint16_t pin) : port_(port), pin_(pin) {}
-    void toggle() { HAL_GPIO_TogglePin(port_, pin_); }
-    void on()    { HAL_GPIO_WritePin(port_, pin_, GPIO_PIN_SET); }
-    void off()   { HAL_GPIO_WritePin(port_, pin_, GPIO_PIN_RESET); }
-private:
-    GPIO_TypeDef* port_;
-    const uint16_t pin_;
-};
 
 extern "C" {
 
