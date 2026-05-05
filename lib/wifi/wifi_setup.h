cc0a26f8c7e908187718e0e7520f3ce5d280747e
Volodymyr Kaliuzhnyi <volodymyr.kaliuzhnyi@globallogic.com>
2026-05-05 16:04:04 +0200

    Fix config
    Signed-off-by: Volodymyr Kaliuzhnyi <volodymyr.kaliuzhnyi@globallogic.com>

--- lib/wifi/wifi_setup.h
+++ lib/wifi/wifi_setup.h
@@ -1,7 +1,7 @@
 #ifndef WIFI_SETUP_H
 #define WIFI_SETUP_H
 
-// Initialises Wi-Fi in STA mode and blocks until connected (or fails).
+// Ініціалізує Wi-Fi в режимі клієнта (STA)
 void wifi_init_sta(void);
 
 #endif // WIFI_SETUP_H
