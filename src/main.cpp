30dd4ce9706f5df4876158152ead2caa13f44fb1
Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>
2026-08-05 19:23:09 +0200

    Add led blinking
    Signed-off-by: Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,27 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
 
-#define LED_PIN     48  // YD-ESP32-S3 onboard WS2812 (IO48)
-#define NUMPIXELS   1   // One RGB LED only
+#define LED_OUT 15
 
-Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+bool ledState = false;
 
 void setup() {
     Serial.begin(115200);
-    pixels.begin();
-    pixels.setBrightness(100); // Brightness (0~255)
-    Serial.println("Rainbow demo start with brightness 100");
-}
+    delay(1000);
 
-uint32_t Wheel(byte pos) {
-    pos = 255 - pos;
-    if(pos < 85) {
-        return pixels.Color(255 - pos * 3, 0, pos * 3);
-    } else if(pos < 170) {
-        pos -= 85;
-        return pixels.Color(0, pos * 3, 255 - pos * 3);
-    } else {
-        pos -= 170;
-        return pixels.Color(pos * 3, 255 - pos * 3, 0);
-    }
+    pinMode(LED_OUT, OUTPUT);
+    digitalWrite(LED_OUT, LOW);
 }
 
 void loop() {
-    for(int i = 0; i < 256; i++) {
-        pixels.setPixelColor(0, Wheel(i));
-        pixels.show();
-        delay(20);
+    if (ledState) {
+        digitalWrite(LED_OUT, LOW);
+        ledState = false;
+        Serial.println("LED OFF");
+    } else {
+        digitalWrite(LED_OUT, HIGH);
+        ledState = true;
+        Serial.println("LED ON");
     }
+
+    delay(1000);
 }
