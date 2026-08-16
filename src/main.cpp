6ef614184e99bf9824b9b5630b6a47d6e49717be
Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>
2026-08-17 00:25:55 +0200

    Add ADC measurement
    Signed-off-by: Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,48 @@
 #include <Arduino.h>
 
 #define LED_PIN PC13
-void setup() {
-    pinMode(LED_PIN, OUTPUT);
-    Serial.begin(115200);
-    while (!Serial) {
+#define ADC_PIN A0
+#define ADC_SAMPLES 16
+#define ADC_RESOLUTION_BITS 12
+#define ADC_MAX_CODE ((1u << ADC_RESOLUTION_BITS) - 1)
+#define ADC_REFERENCE_MV 3300
+
+uint16_t readAveragedAdc(uint8_t sampleCount) {
+    if (sampleCount == 0u) {
+        return 0;
     }
-    Serial.println("UART ready!\r\n");
+
+    uint32_t sum = 0;
+
+    for (uint8_t i = 0; i < sampleCount; ++i) {
+        sum += analogRead(ADC_PIN);
+    }
+
+    return (uint16_t)(sum / sampleCount);
+}
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+
+    analogReadResolution(ADC_RESOLUTION_BITS);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(ADC_PIN, INPUT_ANALOG);
+
+    Serial.println("RAW | mV");
 }
 
 void loop() {
+    const uint16_t raw = readAveragedAdc(ADC_SAMPLES);
+    const float millivolts = raw * ADC_REFERENCE_MV  / (float)ADC_MAX_CODE;
+
     digitalWrite(LED_PIN, HIGH);
+    Serial.print(raw);
+    Serial.print(" | ");
+    Serial.print(millivolts);
+    Serial.println(" mV");
     delay(500);
+
     digitalWrite(LED_PIN, LOW);
     delay(500);
-    Serial.println("LED blinked!");
 }
-
