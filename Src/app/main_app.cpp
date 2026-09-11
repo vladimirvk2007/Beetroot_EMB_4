617d5c163d741cf76b0adabf7da9d0fd8989361d
Volodymyr Kaliuzhnyi <vladimirvk2007@gmail.com>
2026-09-11 17:55:08 +0200

    Add ADC channel

--- Src/app/main_app.cpp
+++ Src/app/main_app.cpp
@@ -3,6 +3,8 @@
 #include "main.h"
 #include "printf/usb_printf.h"
 
+extern "C" ADC_HandleTypeDef hadc1;
+
 extern "C" void main_cpp() {
 
     while(1) {
@@ -11,6 +13,11 @@ extern "C" void main_cpp() {
 	    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
 	    HAL_Delay(500);
 
-        printf("%s", "LED blinked\n");
+        if (HAL_ADC_Start(&hadc1) == HAL_OK) {
+            if (HAL_ADC_PollForConversion(&hadc1, 100) == HAL_OK) {
+                printf("ADC PA5: %lu\n", HAL_ADC_GetValue(&hadc1));
+            }
+            HAL_ADC_Stop(&hadc1);
+        }
     }
 }
